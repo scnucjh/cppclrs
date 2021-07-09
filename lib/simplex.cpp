f24a6c7336f1e5c4b62f11a38985ff51c7f4ea5a
shamirchen <shamirchen@tencent.com>
2021-07-09 20:04:08 +0800

    simplex interface

--- lib/simplex.cpp
+++ lib/simplex.cpp
@@ -1,1 +1,20 @@
 #include "simplex.h"
+
+void simplex::Init(int value_num) {
+  value_num_ = value_num;
+  is_basis_value_ = new bool[value_num_];
+  A_ = new double*[value_num_];
+  for (int i = 0; i < value_num_; i++) {
+    A_[i] = new double[value_num_];
+  }
+}
+
+void simplex::SetA(int i, int j, double value) { A_[i][j] = value; }
+
+void simplex::SetB(int i, double value) { b_[i] = value; }
+
+void simplex::SetC(int i, double value) { c_[i] = value; }
+
+void simplex::SetV(double value) { v_ = value; }
+
+void simplex::Point(int leave, int entry) {}
--- lib/simplex.h
+++ lib/simplex.h
@@ -1,10 +1,27 @@
 #pragma once
 #include <iostream>
+#include <set>
 
 class simplex {
  public:
   static std::string ClassName() { return "simplex"; }
+  void Init(int value_num);
+  void AddBasisValue(int id);
+  void SetA(int i, int j, double value);
+  void SetB(int i, double value);
+  void SetC(int i, double value);
+  void SetV(double value);
+  double run();
+  double run(double *x);
 
- public:
+ private:
   bool *is_basis_value_;
+
+  std::set<int> b_set_;
+
+  int value_num_;
+  double **A_;
+  double *b_;
+  double *c_;
+  double v_;
 };
